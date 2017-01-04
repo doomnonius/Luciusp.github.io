c6731d580db50bd8ce878f334de5f54ed02043ed
Luciusp <lukejperley@gmail.com>
2017-01-04 15:16:55 -0800

    Added basic classes

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-var unitArr = ["gf","pds","spacedock","fighter","destroyer","carrier","cruiser","dreadnaught","warsun"];
-var tiUnit = function(unitName) {console.log("Initiated Unit");};
+var unitNameArr = [["groundforce",8],["pds",6],["spacedock",6],["fighter",9],["destroyer",9],["carrier",9],["cruiser",7],["dreadnought",5],["warsun",3]];
+var numUnits = unitNameArr.length-1;
+var tiUnit = function(unitName,rollVal) { //unit object
+	this.name = unitName;
+	this.roll = rollVal;
+	console.log("Instantiated " + unitName); //confirms all units instantiated
+};
 
-var spacedock = new tiUnit(); //read this please: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript
+var unitList = createUnits(numUnits); //instantiates all unit objects
+
+
+/*----------------------------------*/
 
 var x = 0;
-
-function roller(rollTime) {
+function roller(rollTime) { //rolls number from 0-9 with a delay to make clear it's a new roll
 	setTimeout(function() {var output = Math.floor(Math.random() * 10) + 0;
-	document.getElementById("tester").innerHTML = output;
+	document.getElementById("spacedock_roll0").innerHTML = output;
 	x++;
 		if(x<100) {
 			roller();
@@ -25,4 +32,14 @@ function updateValue() {
 	console.log(nameOf);
 	if (nameOf == 0) {
 	}
+}
+
+function createUnits(n) {
+	var arr = [];
+	while (n >= 0) {
+		arr.push(new tiUnit(unitNameArr[n][0],unitNameArr[n][1]));
+		n--;
+	}
+	console.log('Success!');
+	return arr;
 }
