5da7c41ee0de659e939fba70e801b30619b7166f
Doomnonius <devon.doornbos@gmail.com>
2017-01-12 16:14:29 -0800

    change race select to datafile to enable updateValue() to run on it; give all units 30 dice rolls; hide unused units

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 var unitNameArr = [{name:"spacedock",hitsAt:6,numShots:1},{name:"pds",hitsAt:6,numShots:1},{name:"groundforce",hitsAt:8,numShots:1},{name:"fighter",hitsAt:9,numShots:1},{name:"carrier",hitsAt:9,numShots:1},{name:"cruiser",hitsAt:7,numShots:1},{name:"destroyer",hitsAt:9,numShots:1},{name:"dreadnought",hitsAt:5,numShots:1},{name:"warsun",hitsAt:3,numShots:3}];
 //to add here: first, a switch statement for how each race affects the results based on a selection list in the html file (actually, two lists because some races can penalize their opponents rolls); second, we'll need some way to mark upgrades and action cards and political cards and etc. that can influence battles, and there will be another function here that correctly applies how each one affects the rolls
 function updateValue() {
-	for (var i = 0; i < 9; i++) {
-	unitNameArr[i].numUnits = document.getElementById(unitNameArr[i].name).value;
-	  if (unitNameArr[i].numUnits > 0) {
-	    console.log("Instantiated " + unitNameArr[i].numUnits + " " + unitNameArr[i].name + "s"); //instantiates all unit objects
-	    for (a = 0; a < 9; a++) {
-        unitNameArr[a].allShots = (unitNameArr[a].numShots) * (unitNameArr[a].numUnits);
+	
+	for (var a = 0; a < 9; a++) { //this for loop creates the keys numUnits and allShots and gives them values based on data from the .html for each object in unitNameArr
+	unitNameArr[a].numUnits = document.getElementById(unitNameArr[a].name).value;
+	  if (unitNameArr[a].numUnits > 0) {
+	    console.log("Instantiated " + unitNameArr[a].numUnits + " " + unitNameArr[a].name + "s"); //instantiates all unit objects
+	    unitNameArr[a].allShots = (unitNameArr[a].numShots) * (unitNameArr[a].numUnits);
+	  } else {  //this else loop hides all unit types (images, data entries, and dicerolls) that are not participating in the fight
+      document.getElementById(unitNameArr[a].name).style.display = "none";
+      document.getElementById(unitNameArr[a].name + "_img").setAttribute("class", "datalist");
+      for (var i = 1; i < 31; i++) {
+        document.getElementById(unitNameArr[a].name + "_roll" + i).setAttribute("class", "datalist");
       }
-	  } //else (build some sort of thing here that makes all the unused units disappear)
+	  }
 	}
 }
 
 /*----------------------------------*/
-function roller(rollTime) { //rolls number from 0-9 with a delay to make clear it's a new roll; remember that 0=10, let's keep it like that to reflect what a d10 looks like
+function roller(rollTime) { //rolls number from 1-10 with a delay to make clear it's a new roll
   setTimeout(function() {
-	  var output = Math.floor(Math.random() * 10) + 0;
+	  var output = Math.floor(Math.random() * 10) + 1;
     for (a = 0; a < 9; a++) {
       if (unitNameArr[a].allShots> 0) {
 	      console.log(unitNameArr[a].allShots);
